c9e9aa040391dba6569684b3a24219eb322c6110
Leon Jerke <i19022@hb.dhbw-stuttgart.de>
2021-03-21 18:06:42 +0100

    Aktualisierung der Website

--- website/src/components/pages/Documentpage/Privat.js
+++ website/src/components/pages/Documentpage/Privat.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useAuth0} from "@auth0/auth0-react";
 
 import Aufwandserfassung from '../../../documents/Privat/Apollon_Aufwandserfassung.xlsx';
+import StatusberichtKW10 from '../../../documents/Privat/Apollon_Statusbericht_KW10.pdf';
+import StatusberichtKW11 from '../../../documents/Privat/Apollon_Statusbericht_KW11.pdf';
+
 
 const Privat = () => {
     const { isAuthenticated } = useAuth0();
@@ -9,8 +12,14 @@ const Privat = () => {
         return(
                 <div class="card mt-3" style={{textAlign: "left"}}>
                     <div class="card-body text-align-left">
+                        <h6>Allgemein:</h6>
                         <ul>
                             <li><a href={Aufwandserfassung} >Aufwandserfassung.xlsx</a> <span class="badge badge-dark" style={{background: "#31574B"}}>Neu</span></li>
+                        </ul>
+                        <h6>Statusberichte:</h6>
+                        <ul>
+                            <li><a href={StatusberichtKW10} >Statusbericht_KW10.pdf</a></li>
+                            <li><a href={StatusberichtKW11} >Statusbericht_KW11.pdf</a> <span class="badge badge-dark" style={{background: "#31574B"}}>Neu</span></li>
                         </ul>
                     </div>
                 </div>      
