0a4e48bc2ab366f67b7f6742c2afdab653211761
Paul-Benedict Burkard <i19007@hb.dhbw-stuttgart.de>
2021-04-21 22:07:25 +0200

    Created RegisterPage and RegisterModel for Registration, added Validation for Login and Registration

---
+++ application/Apollon.Mud/Client/Data/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Apollon.Mud.Client.Data.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Bitte gib eine E-Mail")]
+        [EmailAddress(ErrorMessage = "Bitte gib eine gültige E-Mail Adresse ein")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Bitte gib dein Passwort ein!")]
+        [MinLength(5, ErrorMessage = "Dein Passwort muss mindestens 5 Zeichen lang sein")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Bitte gib dein Passwort ein!")]
+        [CompareProperty("Password", ErrorMessage = "Die Passwörter stimmen nicht überein")]
+        public string PasswordVerification { get; set; }
+    }
+}
