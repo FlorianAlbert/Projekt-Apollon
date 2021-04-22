ecce3f67128d712b9723e8a47b9a8c8b73d8ab62
Etienne Zink <etienne.zink@fischer.de>
2021-04-22 21:47:44 +0200

    Added DungeonDto

---
+++ application/Apollon.Mud/Apollon.Mud.Shared/Dungeon/Room/RoomDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollon.Mud.Shared.Dungeon.Room
+{
+    /// <summary>
+    /// ToDo
+    /// </summary>
+    class RoomDto
+    {
+
+    }
+}
