1366d343613d5b704d87f4a36e0e08715484279d
Etienne Zink <etienne.zink@fischer.de>
2021-04-23 20:04:12 +0200

    Updated RoomDto.

--- application/Apollon.Mud/Apollon.Mud.Shared/Dungeon/Room/RoomDto.cs
+++ application/Apollon.Mud/Apollon.Mud.Shared/Dungeon/Room/RoomDto.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Apollon.Mud.Shared.Dungeon.Inspectable;
+using Apollon.Mud.Shared.Dungeon.Requestable;
 using Newtonsoft.Json;
 
 namespace Apollon.Mud.Shared.Dungeon.Room
@@ -23,5 +25,53 @@ namespace Apollon.Mud.Shared.Dungeon.Room
         /// </summary>
         [JsonProperty("Status")]
         public int Status { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("Inspectables")]
+        public ICollection<InspectableDto> Inspectables { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("NeighborNorthId")]
+        public Guid NeighborNorthId { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("NeighborEastId")]
+        public Guid NeighborEastId { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("NeighborSouthId")]
+        public Guid NeighborSouthId { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("NeighborWestId")]
+        public Guid NeighborWestId { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("SpecialActions")]
+        public ICollection<RequestableDto> SpecialActions { get; set; }
     }
 }
