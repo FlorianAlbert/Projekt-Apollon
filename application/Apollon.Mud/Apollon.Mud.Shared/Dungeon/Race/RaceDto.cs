4261baa4c387626f932a23a2f1ac1ce30bea6822
Etienne Zink <etienne.zink@fischer.de>
2021-04-23 20:11:30 +0200

    Updated RaceDto.

--- application/Apollon.Mud/Apollon.Mud.Shared/Dungeon/Race/RaceDto.cs
+++ application/Apollon.Mud/Apollon.Mud.Shared/Dungeon/Race/RaceDto.cs
@@ -23,5 +23,35 @@ namespace Apollon.Mud.Shared.Dungeon.Race
         /// </summary>
         [JsonProperty("Status")]
         public int Status { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("DefaultHealth")]
+        public int DefaultHealth { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("DefaultProtection")]
+        public int DefaultProtection { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("DefaultDamage")]
+        public int DefaultDamage { get; set; }
     }
 }
