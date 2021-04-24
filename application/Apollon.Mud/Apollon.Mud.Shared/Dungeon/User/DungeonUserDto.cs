4557f5c9264cae8d7a94b91bf4191b03a2666384
Etienne Zink <etienne-zink@gmx.de>
2021-04-24 21:46:15 +0200

    Updated DungeonUserDto.

--- application/Apollon.Mud/Apollon.Mud.Shared/Dungeon/User/DungeonUserDto.cs
+++ application/Apollon.Mud/Apollon.Mud.Shared/Dungeon/User/DungeonUserDto.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Apollon.Mud.Shared.Dungeon.User
 {
     /// <summary>
-    /// ToDo
+    /// ToDo ausreichend?!
     /// </summary>
     public class DungeonUserDto
     {
+        /// <summary>
+        /// ToDo serializes DateTime as ISO 8601 standard
+        /// </summary>
+        [JsonProperty("LastActive")]
+        public DateTime LastActive { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("PasswordHash")]
+        public string PasswordHash { get; set; }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("EmailConfirmed")]
+        public bool EmailConfirmed { get; set; }
     }
 }
