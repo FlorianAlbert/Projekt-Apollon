48baf5b6bfcb14a4ab71dfc3af6d5702d6b4d24c
Paul-Benedict Burkard <i19007@hb.dhbw-stuttgart.de>
2021-04-25 14:16:10 +0200

    Fixed formatting in CreateClassComponent with Validation and removed typos

--- application/Apollon.Mud/Client/Data/Models/DungeonClassModel.cs
+++ application/Apollon.Mud/Client/Data/Models/DungeonClassModel.cs
@@ -22,21 +22,21 @@ namespace Apollon.Mud.Client.Data.Models
         /// TODO
         /// </summary>
         [Required(ErrorMessage = "Du musst einen Standard Lebenspunktewert angeben")]
-        [RegularExpression("[0-9]*", ErrorMessage = "Lebenspunkte können nur als Ganzzahl angegeben betragen")]
+        [RegularExpression("[0-9]*", ErrorMessage = "Lebenspunkte können nur eine Ganzzahl sein")]
         public string Health { get; set; }
 
         /// <summary>
         /// TODO
         /// </summary>
         [Required(ErrorMessage = "Du musst einen Standard Schadenswert angeben")]
-        [RegularExpression("[0-9]*", ErrorMessage = "Der Schaden kann nur als Ganzzahl angegeben werden")]
+        [RegularExpression("[0-9]*", ErrorMessage = "Der Schaden kann nur eine Ganzzahl sein")]
         public string Damage { get; set; }
 
         /// <summary>
         /// TODO
         /// </summary>
         [Required(ErrorMessage = "Du musst einen Standard Verteidigungswert angeben")]
-        [RegularExpression("[0-9]*", ErrorMessage = "Die Verteidigung kann nur als Ganzzahl angegeben werden")]
+        [RegularExpression("[0-9]*", ErrorMessage = "Die Verteidigung kann nur eine Ganzzahl sein")]
         public string Protection { get; set; }
 
         /// <summary>
