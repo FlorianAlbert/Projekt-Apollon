148e0e1b8a956fb0b65da8c20534ec3285873d3b
Etienne Zink <etienne-zink@gmx.de>
2021-04-25 15:26:37 +0200

    Added AuthorizationDtos and GameDtos.

---
+++ application/Apollon.Mud/Apollon.Mud.Shared/UserManagement/Authorization/AuthorizationRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Apollon.Mud.Shared.UserManagement.Authorization
+{
+    /// <summary>
+    /// ToDo
+    /// </summary>
+    public class AuthorizationRequestDto
+    {
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("UserEmail")]
+        public string UserEmail { get; set; }
+
+        /// <summary>
+        /// ToDo Hash statt Password versenden?! was braucht das Identity?
+        /// </summary>
+        [JsonProperty("Password")]
+        public string Password { get; set; }
+    }
+}
