6f535cd0064ea86c12be98f04dc4f6ac04b7e397
Etienne Zink <etienne-zink@gmx.de>
2021-04-25 16:25:48 +0200

    Added Dtos for UserManagement.

---
+++ application/Apollon.Mud/Apollon.Mud.Shared/UserManagement/Registration/RegistrationRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Apollon.Mud.Shared.UserManagement.Registration
+{
+    /// <summary>
+    /// ToDo gleiches Dto wie bei AuthorizationRequest. Könnte man nicht beides in ein Dto packen und für beide Anwensungsbereiche verwenden
+    /// </summary>
+    public class RegistrationRequestDto
+    {
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        [JsonProperty("UserEmail")]
+        public string UserEmail { get; set; }
+
+        /// <summary>
+        /// ToDo Hash statt Password versenden?! was braucht das Identity?
+        /// </summary>
+        [JsonProperty("Password")]
+        public string Password { get; set; }
+    }
+}
