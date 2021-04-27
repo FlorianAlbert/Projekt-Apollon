27e60973dc303409aee3dc71b7847a695e9382ff
Paul-Benedict Burkard <i19007@hb.dhbw-stuttgart.de>
2021-04-27 23:55:12 +0200

    Imrpoved folder structure

--- application/Apollon.Mud/Client/Data/Models/Account/ForgotPasswordModel.cs
+++ application/Apollon.Mud/Client/Data/Models/Account/ForgotPasswordModel.cs
@@ -5,10 +5,16 @@ namespace Apollon.Mud.Client.Data.Models
 {
     public class ForgotPasswordModel
     {
+        /// <summary>
+        /// TODO
+        /// </summary>
         [Required(ErrorMessage = "Bitte gib Deine E-Mail Adresse an")]
         [EmailAddress(ErrorMessage = "Bitte gib eine gültige E-Mail Adresse ein")]
         public string Email { get; set; }
 
+        /// <summary>
+        /// TODO
+        /// </summary>
         [Required(ErrorMessage = "Bitte bestätige Deine E-Mail Adresse")]
         [CompareProperty("Email", ErrorMessage = "Die E-Mail Adressen stimmen nicht überein")]
         public string EmailVerification { get; set; }
