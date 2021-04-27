437b86907b12fe5c873306a4d642ecd4c4873b6f
Paul-Benedict Burkard <i19007@hb.dhbw-stuttgart.de>
2021-04-27 22:59:24 +0200

    Implemented ForgotPasswordPage and associated classes

---
+++ application/Apollon.Mud/Client/Data/Models/ForgotPasswordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Apollon.Mud.Client.Data.Models
+{
+    public class ForgotPasswordModel
+    {
+        [Required(ErrorMessage = "Bitte gib Deine E-Mail Adresse an")]
+        [EmailAddress(ErrorMessage = "Bitte gib eine gültige E-Mail Adresse ein")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Bitte bestätige Deine E-Mail Adresse")]
+        [CompareProperty("Email", ErrorMessage = "Die E-Mail Adresse stimmen nicht überein")]
+        public string EmailVerification { get; set; }
+    }
+}
