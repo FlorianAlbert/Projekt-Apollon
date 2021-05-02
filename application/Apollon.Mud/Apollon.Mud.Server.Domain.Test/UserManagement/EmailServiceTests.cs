4eca66aed4c4fe8051d10b57b136dbdc04f89348
Etienne Zink <etienne-zink@gmx.de>
2021-05-02 22:23:39 +0200

    Little change in EmailServiceTest

--- application/Apollon.Mud/Apollon.Mud.Server.Domain.Test/UserManagement/EmailServiceTests.cs
+++ application/Apollon.Mud/Apollon.Mud.Server.Domain.Test/UserManagement/EmailServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using AutoFixture;
 using AutoFixture.AutoNSubstitute;
+using FluentEmail.Core;
+using NSubstitute;
 using Xunit;
 
 namespace Apollon.Mud.Server.Domain.Test.UserManagement
@@ -13,6 +15,12 @@ namespace Apollon.Mud.Server.Domain.Test.UserManagement
         public EmailServiceTests()
         {
             _Fixture = new Fixture().Customize(new AutoNSubstituteCustomization());
+        }
+
+        [Fact]
+        public void SendMail_Succeed()
+        {
+            var fluentMailFactory = Substitute.For<IFluentEmailFactory>();
         }
     }
 }
