5247001dd52858a6a25a60b357a176a6b57f2f89
Leon Jerke <i19022@hb.dhbw-stuttgart.de>
2021-05-07 22:36:52 +0200

    Added AvatarService

---
+++ application/Apollon.Mud/Client/Services/Implementiations/AvatarService.cs
@@ -0,0 +1,114 @@
+﻿using Apollon.Mud.Client.Services.Interfaces;
+using Apollon.Mud.Shared.Dungeon.Avatar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Apollon.Mud.Client.Services.Implementiations
+{
+    public class AvatarService : IAvatarService
+    {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public HttpClient HttpClient { get; }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public CancellationTokenSource CancellationTokenSource { get; }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="httpClient"></param>
+        public AvatarService(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+            CancellationTokenSource = new CancellationTokenSource();
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="avatarDto"></param>
+        /// <param name="dungeonId"></param>
+        /// <returns></returns>
+        public async Task<Guid> CreateNewAvatar(AvatarDto avatarDto, Guid dungeonId)
+        {
+            CancellationToken cancellationToken = CancellationTokenSource.Token;
+
+            var response = await HttpClient.PostAsJsonAsync("api/avatars/{dungeonId}", avatarDto, cancellationToken);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var responseGuid = await response.Content.ReadFromJsonAsync<Guid>();
+                return responseGuid;
+            }
+            return Guid.Empty;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="avatarDto"></param>
+        /// <param name="dungeonId"></param>
+        /// <returns></returns>
+        //public async Task<AvatarDto> UpdateAvatar(AvatarDto avatarDto, Guid dungeonId)
+        //{
+        //    CancellationToken cancellationToken = CancellationTokenSource.Token;
+
+        //    var response = await HttpClient.PutAsJsonAsync("api/avatars/{dungeonId}", avatarDto, cancellationToken);
+
+        //    if (response.StatusCode == HttpStatusCode.OK) return await response.Content.ReadFromJsonAsync<AvatarDto>();
+
+        //    return null;
+        //}
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="avatarId"></param>
+        /// <returns></returns>
+        public Task<bool> DeleteAvatar(Guid avatarId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="dungeonId"></param>
+        /// <returns></returns>
+        public Task<ICollection<AvatarDto>> GetAllAvatars(Guid dungeonId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="dungeonId"></param>
+        /// <returns></returns>
+        public Task<ICollection<AvatarDto>> GetAllAvatarsForUser(Guid dungeonId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="dungeonId"></param>
+        /// <param name="avatarId"></param>
+        /// <returns></returns>
+        public Task<AvatarDto> GetAvatar(Guid dungeonId, Guid avatarId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ application/Apollon.Mud/Client/Services/Interfaces/IAvatarService.cs
@@ -0,0 +1,66 @@
+﻿using Apollon.Mud.Shared.Dungeon.Avatar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Apollon.Mud.Client.Services.Interfaces
+{
+    public interface IAvatarService
+    {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public HttpClient HttpClient { get; }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public CancellationTokenSource CancellationTokenSource { get; }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="avatarDto"></param>
+        /// <param name="dungeonId"></param>
+        /// <returns></returns>
+        Task<Guid> CreateNewAvatar(AvatarDto avatarDto, Guid dungeonId);
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="avatarDto"></param>
+        /// <returns></returns>
+        //Task<AvatarDto> UpdateAvatar(AvatarDto avatarDto);
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="avatarId"></param>
+        /// <returns></returns>
+        Task<bool> DeleteAvatar(Guid avatarId);
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="dungeonId"></param>
+        /// <returns></returns>
+        Task<ICollection<AvatarDto>> GetAllAvatars(Guid dungeonId);
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="dungeonId"></param>
+        /// <returns></returns>
+        Task<ICollection<AvatarDto>> GetAllAvatarsForUser(Guid dungeonId);
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="avatarId"></param>
+        /// <returns></returns>
+        Task<AvatarDto> GetAvatar(Guid dungeonId, Guid avatarId);
+    }
+}
