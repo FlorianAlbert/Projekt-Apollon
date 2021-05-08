142e3581b0b30a906de2dc6cdca3e655a4bbc08b
Etienne Zink <etienne-zink@gmx.de>
2021-05-08 18:15:18 +0200

    Updated tests.

--- application/Apollon.Mud/Server.Model.Test/Dungeon/Avatar/InventoryTests.cs
+++ application/Apollon.Mud/Server.Model.Test/Dungeon/Avatar/InventoryTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using Apollon.Mud.Server.Model.Implementations.Dungeons.Avatars;
 using Apollon.Mud.Server.Model.Implementations.Dungeons.Inspectables.Takeables;
 using AutoFixture;
@@ -23,8 +24,14 @@ namespace Apollon.Mud.Server.Model.Test.Dungeon.Avatar
         [InlineData(0)]
         public void Add_WeightSumUnderMaximum_Success(int newWeight)
         {
-            var takeableMock = _Fixture.Create<Takeable>();
-            takeableMock.Weight = newWeight;
+            var takeableMock = _Fixture.Build<Takeable>()
+                .Without(x => x.Dungeon)
+                .Without(x => x.Rooms)
+                .Without(x => x.HoldingItemAvatars)
+                .Without(x => x.InventoryAvatars)
+                .Without(x => x.Classes)
+                .With(x => x.Weight, newWeight)
+                .Create();
             var inventory = _Fixture.Create<Inventory>();
 
             inventory.Add(takeableMock);
@@ -34,20 +41,35 @@ namespace Apollon.Mud.Server.Model.Test.Dungeon.Avatar
 
         [Theory]
         [InlineData(0, 146)]
-        //[InlineData(63, 72)]
+        [InlineData(63, 72)]
         [InlineData(100, 4)]
         public void Add_WeightSumOverMaximum_Fail(int alreadyContainingWeight, int newWeight)
         {
-            var takeableMock = _Fixture.Create<Takeable>();
-            takeableMock.Weight = newWeight;
-            var inventory = _Fixture.Create<Inventory>();
-            var sum = inventory.WeightSum;
-            sum = alreadyContainingWeight;
+            var takeableMock = _Fixture.Build<Takeable>()
+                .Without(x => x.Dungeon)
+                .Without(x => x.Rooms)
+                .Without(x => x.HoldingItemAvatars)
+                .Without(x => x.InventoryAvatars)
+                .Without(x => x.Classes)
+                .With(x => x.Weight, newWeight)
+                .Create();
+
+            var inventoryItemMock = _Fixture.Build<Takeable>()
+                .Without(x => x.Dungeon)
+                .Without(x => x.Rooms)
+                .Without(x => x.HoldingItemAvatars)
+                .Without(x => x.InventoryAvatars)
+                .Without(x => x.Classes)
+                .With(x => x.Weight, alreadyContainingWeight)
+                .Create();
+
+            var inventory = new Inventory(new List<Takeable>() {inventoryItemMock});
+
 
             inventory.Add(takeableMock);
 
-            //TODO: Kann ja nicht Empty sein, weil der über der maximalen Gewichtauslastung ist.
-            inventory.Should().BeEmpty();
+            
+            inventory.WeightSum.Should().Be(alreadyContainingWeight);
         }
 
         [Fact]
