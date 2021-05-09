d02d8706a4ccfa4f2619a474de0f3d60409b975a
FlorianAlbert <florian21albert@gmx.de>
2021-05-09 15:08:38 +0200

    Improved Extension method

--- application/Apollon.Mud/Server.Model/ModelExtensions/CollectionExtensions.cs
+++ application/Apollon.Mud/Server.Model/ModelExtensions/CollectionExtensions.cs
@@ -6,8 +6,9 @@ namespace Apollon.Mud.Server.Model.ModelExtensions
 {
     public static class CollectionExtensions
     {
-        public static void RemoveAll<T>(this ICollection<T> collection, Func<T, bool> predicate)
+        public static void RemoveAll<T>(this ICollection<T> collection, Func<T, bool> predicate = null)
         {
+            predicate ??= _ => true;
             if (collection is List<T> list) list.RemoveAll(new Predicate<T>(predicate));
             else
             {
